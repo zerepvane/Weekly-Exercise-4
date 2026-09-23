8bed515c492dfbeb65984a7a7b3cfe6a8f4d0217
Vanessa Perez <vperez4957@gmail.com>
2026-09-23 13:40:17 -0500

    answered 7b

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -14,3 +14,23 @@ athletes1992 <- Olympics %>%
   arrange(desc(athletes))
 
 athletes1992
+
+#7b
+
+countries <- c("United States", "France", "Germany", "Russia", "China")
+
+
+goldmedalcountries <- Olympics %>%
+  filter(country %in% countries) %>%
+  ggplot(aes(x = year, y = gold, color = country, group = country)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 2) +
+  labs(
+    title = "Gold Medals Earned Over Time By Country",
+    x = "Year",
+    y = "Number of Gold Medals",
+    color = "Country"
+  ) +
+  theme_minimal()
+
+print(goldmedalcountries)
