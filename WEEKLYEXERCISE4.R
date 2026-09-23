4312f3fcc41e6f8f4377995eff4d09ea2c712f7e
Vanessa Perez <vperez4957@gmail.com>
2026-09-23 13:36:49 -0500

    answered 7a

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -1,3 +1,16 @@
 library(usethis)
 git_default_branch()
 # The name of the branch is "main".
+library(readr)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+
+Olympics <- read_csv("Olympics.csv")
+
+athletes1992 <- Olympics %>%
+  filter(year == 1992) %>%
+  select(country, athletes) %>% 
+  arrange(desc(athletes))
+
+athletes1992
