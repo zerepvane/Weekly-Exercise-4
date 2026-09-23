f730fa73f07e789903429d66a893f8f48ca7c3c2
Vanessa Perez <vperez4957@gmail.com>
2026-09-23 13:34:29 -0500

    ignore previous commmit, realized it was wrong branch to send to. This is how DEV should look like as of rn

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -1,6 +1,5 @@
 library(usethis)
 git_default_branch()
-# The name of the branch is "main".
 library(readr)
 library(tidyverse)
 library(ggplot2)
@@ -8,10 +7,34 @@ library(dplyr)
 
 Olympics <- read_csv("Olympics.csv")
 
-athletes1992 <- Olympics %>%
-  filter(year == 1992) %>%
-  select(country, athletes) %>%
-  arrange(desc(athletes))
-athletes1992
+newOlympics <- Olympics %>%
+  pivot_wider(
+    id_cols = country,
+    names_from = year,
+    values_from = c(gold, silver, bronze),
+    values_fill = 0
+  )
 
+# total medals
+newOlympics <- newOlympics %>%
+  mutate(total.medals=rowSums(across(starts_with(c("gold","silver","bronze"))),na.rm = TRUE)
+  )
+newOlympics[,c("country", "total.medals")]
+
+# gold medals
+newOlympics <- newOlympics %>%
+  mutate(gold.medals=rowSums(across(starts_with(c("gold"))),na.rm = TRUE)
+  )
+newOlympics[,c("country", "gold.medals")]
+
+# medal total by year
+total_medals_by_year <- Olympics %>%
+  group_by(year) %>%
+  summarise(
+    TotalGold = sum(gold, na.rm = TRUE),
+    TotalSilver = sum(silver, na.rm = TRUE),
+    TotalBronze = sum(bronze, na.rm = TRUE),
+    TotalByYear = sum(gold + silver + bronze, na.rm = TRUE)
+  )
+print(total_medals_by_year)
 
